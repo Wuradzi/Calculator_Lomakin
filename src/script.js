13dcfbeaa0ce471f7c9710ab25dbfdf063a37bd4
Wuradzi <wuradzi@gmail.com>
2025-03-25 21:02:37 +0200

    Додано функцію відкриття та закриття дужок

--- src/script.js
+++ src/script.js
@@ -7,6 +7,11 @@ function appendNumber(number) {
     updateResult();
 }
 
+function appendCharacter(char) {
+    currentInput += char;
+    updateResult();
+}
+
 function setOperation(op) {
     if (currentInput === '') return;
     if (previousInput !== '') calculateResult();
@@ -16,22 +21,10 @@ function setOperation(op) {
 }
 
 function calculateResult() {
-    if (operation === null || currentInput === '' || previousInput === '') return;
-    const num1 = parseFloat(previousInput);
-    const num2 = parseFloat(currentInput);
-    switch (operation) {
-        case '+':
-            currentInput = (num1 + num2).toString();
-            break;
-        case '-':
-            currentInput = (num1 - num2).toString();
-            break;
-        case '*':
-            currentInput = (num1 * num2).toString();
-            break;
-        case '/':
-            currentInput = (num2 !== 0 ? (num1 / num2).toString() : 'Error');
-            break;
+    try {
+        currentInput = eval(currentInput).toString();
+    } catch (e) {
+        currentInput = 'Error';
     }
     operation = null;
     previousInput = '';
